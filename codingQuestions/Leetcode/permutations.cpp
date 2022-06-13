959c4953723b5c0feab01e377536b8ea9bfd189c
Harsh Agarwal <harshu07022002@gmail.com>
2022-06-13 19:13:44 +0530

    new code

---
+++ codingQuestions/Leetcode/permutations.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<vector<int>> ans;
+    void permutation(vector<int> &nums, int i)
+    {
+        if (i == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for (int j = i; j < nums.size(); j++)
+        {
+            swap(nums[i], nums[j]);
+            permutation(nums, i + 1);
+            swap(nums[i], nums[j]);
+        }
+        return;
+    }
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        permutation(nums, 0);
+        return ans;
+    }
+};
